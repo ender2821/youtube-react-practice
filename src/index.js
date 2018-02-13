966e97c1e5b887925482e0456b860773d095a0c5
Josh Jensen <design@joshjensencreative.com>
2018-02-13 13:25:03 -0600

    added data to react, and created an array of components for the video list.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar.js';
-
+import VideoList from './components/video-list.js';
 const API_KEY = 'AIzaSyDvEHmgS_GO-OLGj8qEqq-XbOs4T3ZFxdQ';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+
+class App extends Component{
+	constructor(props){
+		super(props)
+		this.state = { videos: [] };
+		
+		YTSearch({key: API_KEY, term: 'ducati'}, (videos) => {
+			this.setState({videos});
+			// this.setState({viedos: videos}); This ES6 resovesto this 
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDom.render(<App />, document.querySelector('.container'))
