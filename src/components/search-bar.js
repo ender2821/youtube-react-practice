1f005521cfc2b115c94b60a71a1747067bc05c68
Josh Jensen <design@joshjensencreative.com>
2018-02-14 16:45:27 -0600

    added search functionality and delay in fetching data

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -11,9 +11,15 @@ class SearchBar extends Component {
       <div className='search-bar'>
         <input 
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value }) }/>
+          onChange={event => this.onInputChange(event.target.value)}/>
       </div>
     );
+  }
+
+  onInputChange(term) {
+  	this.setState({term});
+  	this.props.onSearchTermChange(term);
+  	console.log('term',term);
   }
 }
 
